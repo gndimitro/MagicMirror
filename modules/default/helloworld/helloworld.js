e1f47f074299174e5350265febe736cc67cca9e5
George <george.n.dimitrov@gmail.com>
2016-08-08 18:38:02 -0600

    Learned how to animate hello world, just for knowledge

--- modules/default/helloworld/helloworld.js
+++ modules/default/helloworld/helloworld.js
@@ -6,18 +6,35 @@
  * By Michael Teeuw http://michaelteeuw.nl
  * MIT Licensed.
  */
+var worldText = "Hello"
 
 Module.register("helloworld",{
 
 	// Default module config.
 	defaults: {
-		text: "Hello World!"
+		text: "Hello World!",
+		updateInterval: 30000,
+		fadeSpeed: 4000
+	},
+
+	start: function() {
+		// Schedule update timer.
+		var self = this;
+		setInterval(function() {
+			if(worldText == "Hello") {
+				worldText = "World"
+			}
+			else {
+				worldText = "Hello"
+			}
+			self.updateDom(self.config.fadeSpeed);
+		}, this.config.updateInterval);
 	},
 
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
+		wrapper.innerHTML = worldText;
 		return wrapper;
 	}
 });
